004bc12b89c786f58181b38f5cffea7c8e29ca5c
Matt Whalley <matt@mattwhalley.com>
2025-01-07 09:58:06 -0500

    feat: update start script to run Remix app and Storybook server concurrently

--- src/simple-integration-app-client/start.sh
+++ src/simple-integration-app-client/start.sh
@@ -1,2 +1,14 @@
-COPY start.sh ./
-RUN chmod +x start.sh
+#!/bin/sh
+
+# Start Remix app in the background
+node build/server/index.js &
+
+# Start Storybook static server in the background
+# Using port 6006 which is Storybook's default port
+serve -s storybook-static -l 6006 &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
