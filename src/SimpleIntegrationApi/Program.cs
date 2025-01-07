43692c410eba81324fd93a63837497aadb06ad35
Matt Whalley <matt@mattwhalley.com>
2025-01-07 11:01:55 -0500

    fix: conditionally enable HTTPS redirection based on environment in SimpleIntegrationApi

--- src/SimpleIntegrationApi/Program.cs
+++ src/SimpleIntegrationApi/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.   
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
@@ -21,15 +20,8 @@ builder.Services.AddHttpClient<NppesApiClient>(client =>
 {
     client.BaseAddress = new Uri("https://npiregistry.cms.hhs.gov");
 });
-builder.Services.AddHttpsRedirection(options =>
-{
-    options.HttpsPort = 443;
-    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
-});
 
 var app = builder.Build();
-
-app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -38,10 +30,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Only use HTTPS redirection if not behind a proxy
+if (!app.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.HttpsPort = 443;
+        options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+    });
+}
 
-app.UseAuthorization();
 app.UseRouting();
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
