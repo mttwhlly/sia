a57586e0c884255c032225c4b951fa0db936041c
Matt Whalley <matt@mattwhalley.com>
2025-03-13 12:20:29 -0400

    Add Cors namespace via DI

--- src/SimpleIntegrationApi/Controllers/ProvidersController.cs
+++ src/SimpleIntegrationApi/Controllers/ProvidersController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SimpleIntegrationApi.Mappers;
 using SimpleIntegrationApi.Services;
+using Microsoft.AspNetCore.Cors;
 
 namespace SimpleIntegrationApi.Controllers;
 
